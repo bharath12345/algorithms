d2132ae33406d3815d300abc2dabd0baaf5abd52
bharath12345 <bharath12345@gmail.com>
2013-10-03 12:16:31 +0530

    starting on assignment 5

---
+++ kdtree/PointSET.java
@@ -0,0 +1,76 @@
+package kdtree;
+
+import algs4.*;
+
+/**
+ * User: bharadwaj
+ * Date: 03/10/13
+ * Time: 11:57 AM
+ */
+public class PointSET {
+
+    /**
+     * construct an empty set of points
+     */
+    public PointSET() {
+
+    }
+
+    /**
+     * is the set empty?
+     * @return
+     */
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * number of points in the set
+     * @return
+     */
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * add the point p to the set (if it is not already in the set)
+     * @param p
+     */
+    public void insert(Point2D p) {
+
+    }
+
+    /**
+     * does the set contain the point p?
+     * @param p
+     * @return
+     */
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    /**
+     * draw all of the points to standard draw
+     */
+    public void draw() {
+
+    }
+
+    /**
+     * all points in the set that are inside the rectangle
+     * @param rect
+     * @return
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    /**
+     * a nearest neighbor in the set to p; null if set is empty
+     * @param p
+     * @return
+     */
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+}
